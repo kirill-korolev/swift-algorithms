cbe156e14a5e8acc4a19015a94492d13b1674fb0
kirill-korolev <kafafyf@gmail.com>
2017-02-21 22:56:27 +0300

    involution added

--- Involution.playground/Contents.swift
+++ Involution.playground/Contents.swift
@@ -2,16 +2,28 @@
 
 import UIKit
 
-func involute(_ a: Int, in power: Int){
+func involute(_ a: Int, in power: Int) -> Int{
     
     var a0 = a
+    var powers = [[Int]]()
+    
+    powers.append([1, a0])
     
     for i in 1..<power where i % 2 == 0{
         a0 *= a0
-        
+        powers.append([i, a0])
     }
+    
+    let maxPower = powers[powers.count-1][0]
+    let modPower = power - maxPower
+    
+    if modPower != 0{
+        return powers[powers.count-1][1]*powers[1][1]
+    }
+    
+    return powers[powers.count-1][1]
 }
 
 let a = 7
-involute(a, in: 6)
+print(involute(a, in: 6))
 
